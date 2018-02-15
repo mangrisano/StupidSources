98bd4055e49d9234770ff10bd8f40e89eb5934eb
Michele Angrisano <darksun@Zerstorer.local>
2018-02-15 18:22:30 +0100

    Added score and some features

--- tictactoe.py
+++ tictactoe.py
@@ -7,20 +7,18 @@
 
 from random import choice
 
-board = list(range(1, 10))
-
 def check_line(board, player):
     # win = 0
     status = 1
     if (board[0] == board[1] == board[2]) or \
-       (board[3] == board[4] == board[5]) or \
-       (board[6] == board[7] == board[8]) or \
-       (board[0] == board[3] == board[6]) or \
-       (board[1] == board[4] == board[7]) or \
-       (board[2] == board[5] == board[8]) or \
-       (board[0] == board[4] == board[8]) or \
-       (board[2] == board[4] == board[6]):
-        status = 0
+            (board[3] == board[4] == board[5]) or \
+            (board[6] == board[7] == board[8]) or \
+            (board[0] == board[3] == board[6]) or \
+            (board[1] == board[4] == board[7]) or \
+            (board[2] == board[5] == board[8]) or \
+            (board[0] == board[4] == board[8]) or \
+            (board[2] == board[4] == board[6]):
+                status = 0
     return status
 
 def print_board(board):
@@ -52,26 +50,34 @@ def moves(board, player, how_many=2):
     else:
         is_wrong = True
         print("Invalid move. Choose another one!")
-        
+
     return move, is_wrong
 
 def main():
+    p1_score = 0
+    p2_score = 0
+    next_game = True
     print("How many players? ", end="")
     how_many = int(input())
-    print_board(board)
+    if how_many > 2 or how_many < 1:
+        print("Invalid number of players. Choose between 1 or 2 player! (1 player you will play with the Computer).")
+        return
     while True:
+        print("Game is starting...")
+        board = list(range(1, 10))
         list_of_moves = []
         player = 1
-        while True:
+        print_board(board)
+        while next_game:
             print()
             print("Player " + str(player) + ": Is your turn!")
             move, is_wrong = moves(board, player, how_many)
+            print_board(board)
             if move in list_of_moves:
                 continue
             else:
                 list_of_moves.append(move)
             status = check_line(board, player)
-            print_board(board)
             if status == 1 and len(list_of_moves) < 9:
                 if player == 1 and not is_wrong:
                     player = 2
@@ -80,10 +86,26 @@ def main():
                         player = 1
             else:
                 if status == 0:
+                    if player == 1:
+                        p1_score += 1
+                    else:
+                        p2_score += 1
                     print("Player " + str(player) + " wins!")
-                    return
-                if len(list_of_moves) == 9:
+                    print("Score: Player1 = ", p1_score)
+                    print("Score: Player2 = ", p2_score)
+                elif len(list_of_moves) == 9:
                     print("Draw!")
+
+                game = input("Do you wanna play again? (Y for continuing, N for stopping): ")
+                if game not in ['Y', 'N']:
+                    print("Invalid argument!")
+                    game = input("Do you wanna play again? (Y for continuing, N for stopping): ")
+                if game == 'N':
+                    print("Game Over")
+                    next_game = False
                     return
+                elif game == 'Y':
+                    break 
+                        
 
 main()
